cf7c79b8bb517f0cb60ac67bb29598552051f8df
CFDcodesBPS <bhanu2204@gmail.com>
2023-05-22 19:09:47 +0200

    Rotation Rate tensor using RANS computed velocity field

---
+++ Strain_rate_tensor/Strain_rate_tensor.C
@@ -0,0 +1,84 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) 2013-2018 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "Strain_rate_tensor.H"
+#include "fvcGrad.H"
+#include "addToRunTimeSelectionTable.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+namespace functionObjects
+{
+    defineTypeNameAndDebug(Strain_rate_tensor, 0);
+
+    addToRunTimeSelectionTable
+    (
+        functionObject,
+        Strain_rate_tensor,
+        dictionary
+    );
+}
+}
+
+
+// * * * * * * * * * * * * * Private Member Functions  * * * * * * * * * * * //
+
+bool Foam::functionObjects::Strain_rate_tensor::calc()
+{
+        const volVectorField& Ucopy = lookupObject<volVectorField>("U");
+        const tmp<volTensorField> tgradU(fvc::grad(Ucopy));
+        const volTensorField& gradU = tgradU();
+	const volScalarField magS = magSqr(symm(gradU));
+	return store
+        (
+            resultName_, magS * 1
+        );
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::functionObjects::Strain_rate_tensor::Strain_rate_tensor
+(
+    const word& name,
+    const Time& runTime,
+    const dictionary& dict
+)
+:
+    fieldExpression(name, runTime, dict, "U")
+{
+    setResultName(typeName, "U");
+}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+Foam::functionObjects::Strain_rate_tensor::~Strain_rate_tensor()
+{}
+
+
+// ************************************************************************* //
